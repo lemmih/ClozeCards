71ea545512b7a064b4782d42f949f1033c2a934c
David Himmelstrup <lemmih@gmail.com>
2017-11-17 22:29:08 +0100

    Pretty-print js.

--- frontend/app/src/components/deck-body.js
+++ frontend/app/src/components/deck-body.js
@@ -1,43 +1,51 @@
-import _ from 'lodash'
-import React, { Component } from 'react'
-import {
-  Sticky, Grid, Icon, Button, Dropdown
-} from 'semantic-ui-react'
-import { EditorState, convertToRaw, convertFromRaw } from 'draft-js'
-import Editor from 'draft-js-plugins-editor'
-import 'draft-js-static-toolbar-plugin/lib/plugin.css'
+import _ from "lodash";
+import React, { Component } from "react";
+import { Sticky, Grid, Icon, Button, Dropdown } from "semantic-ui-react";
+import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
+import Editor from "draft-js-plugins-editor";
+import "draft-js-static-toolbar-plugin/lib/plugin.css";
 
-import createChinesePlugin from '../plugins/chinese-plugin.js'
-import createResizePlugin from '../plugins/resize'
-import alignContent from '../align.js'
-import mkToolbar from './toolbar'
-import Loading from './loading'
+import createChinesePlugin from "../plugins/chinese-plugin.js";
+import createResizePlugin from "../plugins/resize";
+import alignContent from "../align.js";
+import mkToolbar from "./toolbar";
+import Loading from "./loading";
 
 class DeckBody extends Component {
   constructor(props) {
     super(props);
-    if( props.deckContent )
+    if (props.deckContent)
       this.state = {
-        selection: 'self',
-        deckEditor: EditorState.createWithContent(convertFromRaw(props.deckContent)),
+        selection: "self",
+        deckEditor: EditorState.createWithContent(
+          convertFromRaw(props.deckContent)
+        ),
         // deckEditor: EditorState.createEmpty(),
-        annotationEditor: EditorState.createEmpty(),
+        annotationEditor: EditorState.createEmpty()
       };
     else
       this.state = {
-        selection: 'self',
+        selection: "self",
         deckEditor: EditorState.createEmpty(),
         annotationEditor: EditorState.createEmpty()
       };
     this.deckEditorFocus = () => this.deckEditor.focus();
     this.annotationEditorFocus = () => this.annotationEditor.focus();
-    this.deckEditorPlugins = [ createChinesePlugin(), createResizePlugin(this.alignText) ];
-    this.annotationEditorPlugins = [ createChinesePlugin(), createResizePlugin(this.alignText) ];
+    this.deckEditorPlugins = [
+      createChinesePlugin(),
+      createResizePlugin(this.alignText)
+    ];
+    this.annotationEditorPlugins = [
+      createChinesePlugin(),
+      createResizePlugin(this.alignText)
+    ];
   }
   initialize = () => {
-    if( this.props.deckContent )
+    if (this.props.deckContent)
       this.setState({
-        deckEditor: EditorState.createWithContent(convertFromRaw(this.props.deckContent)),
+        deckEditor: EditorState.createWithContent(
+          convertFromRaw(this.props.deckContent)
+        ),
         annotationEditor: EditorState.createEmpty()
       });
     else
@@ -45,128 +53,168 @@ class DeckBody extends Component {
         deckEditor: EditorState.createEmpty(),
         annotationEditor: EditorState.createEmpty()
       });
-  }
+  };
 
   editorStyles = {
     minHeight: "10em",
     backgroundColor: "white"
-  }
-  handleRef = (ref) => this.setState({ref})
+  };
+  handleRef = ref => this.setState({ ref });
 
   deckEditorRaw = () => {
-    return convertToRaw(this.state.deckEditor.getCurrentContent())
-  }
-
+    return convertToRaw(this.state.deckEditor.getCurrentContent());
+  };
 
   alignText = _.throttle(() => {
-    if( this.props.showAnnotations )
-      alignContent(this.deckEditor.editor, this.annotationEditor.editor);}
-  , 250)
+    if (this.props.showAnnotations)
+      alignContent(this.deckEditor.editor, this.annotationEditor.editor);
+  }, 250);
 
-  handleDeckEditorChange = deckEditor => this.setState({deckEditor});
-  handleAnnotationEditorChange = annotationEditor => this.setState({annotationEditor});
+  handleDeckEditorChange = deckEditor => this.setState({ deckEditor });
+  handleAnnotationEditorChange = annotationEditor =>
+    this.setState({ annotationEditor });
 
-  handleSelectionChange = (e, {value}) => {
-    this.setState({selection: value});
-  }
+  handleSelectionChange = (e, { value }) => {
+    this.setState({ selection: value });
+  };
 
   render = () => {
     const { editNotes } = this.props;
     const showAnnotations = !!this.props.showAnnotations;
-    const mayEditNotes = (this.state.selection === 'self' || !this.state.selection);
+    const mayEditNotes =
+      this.state.selection === "self" || !this.state.selection;
     const editDeck = this.props.editDeck;
     const anyEditable = editDeck || (showAnnotations && editNotes);
     const Toolbar = editDeck
-                    ? mkToolbar(this.state.deckEditor, this.handleDeckEditorChange)
-                    : mkToolbar(this.state.annotationEditor, this.handleAnnotationEditorChange);
+      ? mkToolbar(this.state.deckEditor, this.handleDeckEditorChange)
+      : mkToolbar(
+          this.state.annotationEditor,
+          this.handleAnnotationEditorChange
+        );
 
-    const options =
-            [{ value: 'self', text: 'Personal Notes'}
-            ,{ value: 'ching', text: 'Author\'s Notes'}
-            ,{ value: 'david', text: 'David\'s Notes'}];
+    const options = [
+      { value: "self", text: "Personal Notes" },
+      { value: "ching", text: "Author's Notes" },
+      { value: "david", text: "David's Notes" }
+    ];
 
-    return(
+    return (
       <Loading active>
-      <div>
-        { showAnnotations && !editNotes && <Grid>
-          <Grid.Row>
-            <Grid.Column width={10}>
-            </Grid.Column>
-            <Grid.Column width={6}>
-              <Dropdown
-                placeholder="Viewing notes for: you"
-                search
-                value={this.state.selection}
-                selection
-                selectOnNavigation={false}
-                options={options}
-                onChange={this.handleSelectionChange}/>
-              { mayEditNotes
-              ? <a onClick={this.props.onEditNotes}><Icon name="write" size="big"/></a>
-              : <Icon name="write" size="big"/> }
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>}
-        <div ref={this.handleRef}>
-          { anyEditable && <Sticky context={this.state.ref} offset={20} className="sticky">
-            <div className="toolbar">
-              <a onClick={this.props.onSave}><Icon size="big" name="save"/></a>
-              <Button.Group compact basic size="big" onMouseDown={event => event.preventDefault()}>
-                <Toolbar.Inline type="ITALIC" icon="italic"/>
-                <Toolbar.Inline type="BOLD" icon="bold"/>
-                <Toolbar.Inline type="UNDERLINE" icon="underline"/>
-                <Toolbar.Inline type="CODE" icon="code"/>
-              </Button.Group>
-              {' - '}
-              <Button.Group compact basic size="big" onMouseDown={event => event.preventDefault()}>
-                <Toolbar.Block blockType='code-block' icon="terminal"/>
-                <Toolbar.Block blockType='header-one' icon="header"/>
-                <Toolbar.Block blockType='header-two'>
-                  <Icon size="small" name="header"/>
-                </Toolbar.Block>
-                <Toolbar.Block blockType='header-three'>
-                  <Icon size="tiny" name="header"/>
-                </Toolbar.Block>
-                <Toolbar.Block blockType='unordered-list-item' icon="unordered list"/>
-                <Toolbar.Block blockType='ordered-list-item' icon="ordered list"/>
-                <Toolbar.Block blockType='blockquote' icon="quote left"/>
-              </Button.Group>
-              <a onClick={this.props.onDiscard}><Icon size="big" name="trash"/></a>
-            </div>
-          </Sticky> }
-
-          <div className="editor">
-            <Grid padded={false} celled="internally">
-              <Grid.Row>
-                <Grid.Column width={showAnnotations ? 10 : 16} onClick={this.deckEditorFocus}>
-                  <Editor
-                    editorState={this.state.deckEditor}
-                    onChange={this.handleDeckEditorChange}
-                    plugins={this.deckEditorPlugins}
-                    placeholder="Deck content..."
-                    readOnly={!editDeck}
-                    ref={(ref) => this.deckEditor = ref}
-                  />
-                </Grid.Column>
-                { showAnnotations &&
-                  <Grid.Column width={6} onClick={this.annotationEditorFocus}>
-                    <Editor
-                      editorState={this.state.annotationEditor}
-                      onChange={this.handleAnnotationEditorChange}
-                      plugins={this.annotationEditorPlugins}
-                      placeholder={editNotes ? "Write personal notes here." : ""}
-                      readOnly={!editNotes}
-                      ref={(ref) => this.annotationEditor = ref}
+        <div>
+          {showAnnotations &&
+            !editNotes && (
+              <Grid>
+                <Grid.Row>
+                  <Grid.Column width={10} />
+                  <Grid.Column width={6}>
+                    <Dropdown
+                      placeholder="Viewing notes for: you"
+                      search
+                      value={this.state.selection}
+                      selection
+                      selectOnNavigation={false}
+                      options={options}
+                      onChange={this.handleSelectionChange}
                     />
-                  </Grid.Column> }
-              </Grid.Row>
-            </Grid>
+                    {mayEditNotes ? (
+                      <a onClick={this.props.onEditNotes}>
+                        <Icon name="write" size="big" />
+                      </a>
+                    ) : (
+                      <Icon name="write" size="big" />
+                    )}
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            )}
+          <div ref={this.handleRef}>
+            {anyEditable && (
+              <Sticky context={this.state.ref} offset={20} className="sticky">
+                <div className="toolbar">
+                  <a onClick={this.props.onSave}>
+                    <Icon size="big" name="save" />
+                  </a>
+                  <Button.Group
+                    compact
+                    basic
+                    size="big"
+                    onMouseDown={event => event.preventDefault()}
+                  >
+                    <Toolbar.Inline type="ITALIC" icon="italic" />
+                    <Toolbar.Inline type="BOLD" icon="bold" />
+                    <Toolbar.Inline type="UNDERLINE" icon="underline" />
+                    <Toolbar.Inline type="CODE" icon="code" />
+                  </Button.Group>
+                  {" - "}
+                  <Button.Group
+                    compact
+                    basic
+                    size="big"
+                    onMouseDown={event => event.preventDefault()}
+                  >
+                    <Toolbar.Block blockType="code-block" icon="terminal" />
+                    <Toolbar.Block blockType="header-one" icon="header" />
+                    <Toolbar.Block blockType="header-two">
+                      <Icon size="small" name="header" />
+                    </Toolbar.Block>
+                    <Toolbar.Block blockType="header-three">
+                      <Icon size="tiny" name="header" />
+                    </Toolbar.Block>
+                    <Toolbar.Block
+                      blockType="unordered-list-item"
+                      icon="unordered list"
+                    />
+                    <Toolbar.Block
+                      blockType="ordered-list-item"
+                      icon="ordered list"
+                    />
+                    <Toolbar.Block blockType="blockquote" icon="quote left" />
+                  </Button.Group>
+                  <a onClick={this.props.onDiscard}>
+                    <Icon size="big" name="trash" />
+                  </a>
+                </div>
+              </Sticky>
+            )}
+
+            <div className="editor">
+              <Grid padded={false} celled="internally">
+                <Grid.Row>
+                  <Grid.Column
+                    width={showAnnotations ? 10 : 16}
+                    onClick={this.deckEditorFocus}
+                  >
+                    <Editor
+                      editorState={this.state.deckEditor}
+                      onChange={this.handleDeckEditorChange}
+                      plugins={this.deckEditorPlugins}
+                      placeholder="Deck content..."
+                      readOnly={!editDeck}
+                      ref={ref => (this.deckEditor = ref)}
+                    />
+                  </Grid.Column>
+                  {showAnnotations && (
+                    <Grid.Column width={6} onClick={this.annotationEditorFocus}>
+                      <Editor
+                        editorState={this.state.annotationEditor}
+                        onChange={this.handleAnnotationEditorChange}
+                        plugins={this.annotationEditorPlugins}
+                        placeholder={
+                          editNotes ? "Write personal notes here." : ""
+                        }
+                        readOnly={!editNotes}
+                        ref={ref => (this.annotationEditor = ref)}
+                      />
+                    </Grid.Column>
+                  )}
+                </Grid.Row>
+              </Grid>
+            </div>
           </div>
         </div>
-      </div>
       </Loading>
     );
-  }
+  };
 }
 
 export default DeckBody;
--- frontend/app/src/pages/decks.js
+++ frontend/app/src/pages/decks.js
@@ -1,23 +1,21 @@
-import _ from 'lodash'
-import React, { Component } from 'react'
-import { withRouter, Redirect } from 'react-router-dom'
-import { connect } from 'react-redux'
-import {
-  Container, Item, Loader
-} from 'semantic-ui-react'
-import uuid from 'uuid/v4'
+import _ from "lodash";
+import React, { Component } from "react";
+import { withRouter, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { Container, Item, Loader } from "semantic-ui-react";
+import uuid from "uuid/v4";
 
-import DeckHeader from '../components/deck-header'
-import DeckBody from '../components/deck-body'
-import DeckComments from '../components/deck-comments'
-import Study from '../components/deck-study'
+import DeckHeader from "../components/deck-header";
+import DeckBody from "../components/deck-body";
+import DeckComments from "../components/deck-comments";
+import Study from "../components/deck-study";
 
-import {receiveDeck, fetchDeck} from '../actions/decks'
-import {receiveContent, fetchContent} from '../actions/content'
+import { receiveDeck, fetchDeck } from "../actions/decks";
+import { receiveContent, fetchContent } from "../actions/content";
 
-import mkSlug from '../misc/slugs'
+import mkSlug from "../misc/slugs";
 
-import backend from '../backend'
+import backend from "../backend";
 
 // states:
 //   not fetched => doesn't have key
@@ -33,123 +31,128 @@ function toViewDeckProps(store, ownProps) {
     content: content
   };
 }
-export const ViewDeck = connect(toViewDeckProps)(class ViewDeck extends Component {
-  constructor(props) {
-    super(props);
-    const {study, notes} = props;
-    this.state = { editing: '', study, notes };
-    // console.log('ViewDeck Constructor', !!study, !!notes);
-  }
-  componentWillReceiveProps = (nextProps) => {
-    // console.log('Will receive', nextProps);
-    if( nextProps.study === undefined && nextProps.notes === undefined)
-      this.setState({study: false, notes: false});
-    if(nextProps.study !== undefined)
-      this.setState({study: nextProps.study});
-    if(nextProps.notes !== undefined)
-      this.setState({notes: nextProps.notes});
-  }
-
-  handleEdit = () => {
-    this.setState({ editing: 'deck' });
-  }
-  handleEditNotes = () => {
-    this.setState({ editing: 'notes' });
-  }
-  handleChange = deck => {
-    this.setState({ deck });
-  }
-  handleSave = () => {
-    switch (this.state.editing) {
-      case 'deck':
-        const deck = this.state.deck || this.props.deck;
-        const slug = mkSlug(deck.title);
-        const slugs = _.uniq([slug].concat(deck.slugs));
-        const contentId = uuid();
-
-
-        backend.relay(receiveContent(contentId, this.bodyRef.deckEditorRaw()));
-        backend.relay(receiveDeck({...deck, slugs, contentId}));
-
-        this.setState({ editing: '' });
-        return;
-      case 'notes':
-        this.setState({ editing: '' });
-        return;
+export const ViewDeck = connect(toViewDeckProps)(
+  class ViewDeck extends Component {
+    constructor(props) {
+      super(props);
+      const { study, notes } = props;
+      this.state = { editing: "", study, notes };
+      // console.log('ViewDeck Constructor', !!study, !!notes);
     }
-  }
-  handleDiscard = () => {
-    this.bodyRef.initialize();
-    this.setState({ editing: '', deck: undefined });
-  }
-  handleBodyRef = (ref) => this.bodyRef = ref;
+    componentWillReceiveProps = nextProps => {
+      // console.log('Will receive', nextProps);
+      if (nextProps.study === undefined && nextProps.notes === undefined)
+        this.setState({ study: false, notes: false });
+      if (nextProps.study !== undefined)
+        this.setState({ study: nextProps.study });
+      if (nextProps.notes !== undefined)
+        this.setState({ notes: nextProps.notes });
+    };
 
-  componentDidMount() {
-    if( _.isUndefined(this.props.deck) ) {
-      backend.relay(fetchDeck(this.props.slug));
-    }
-    if(_.isPlainObject(this.props.deck)) {
-      if( _.isUndefined(this.props.content) ) {
-        backend.relay(fetchContent(this.props.deck.contentId));
+    handleEdit = () => {
+      this.setState({ editing: "deck" });
+    };
+    handleEditNotes = () => {
+      this.setState({ editing: "notes" });
+    };
+    handleChange = deck => {
+      this.setState({ deck });
+    };
+    handleSave = () => {
+      switch (this.state.editing) {
+        case "deck":
+          const deck = this.state.deck || this.props.deck;
+          const slug = mkSlug(deck.title);
+          const slugs = _.uniq([slug].concat(deck.slugs));
+          const contentId = uuid();
+
+          backend.relay(
+            receiveContent(contentId, this.bodyRef.deckEditorRaw())
+          );
+          backend.relay(receiveDeck({ ...deck, slugs, contentId }));
+
+          this.setState({ editing: "" });
+          return;
+        case "notes":
+          this.setState({ editing: "" });
+          return;
+      }
+    };
+    handleDiscard = () => {
+      this.bodyRef.initialize();
+      this.setState({ editing: "", deck: undefined });
+    };
+    handleBodyRef = ref => (this.bodyRef = ref);
+
+    componentDidMount() {
+      if (_.isUndefined(this.props.deck)) {
+        backend.relay(fetchDeck(this.props.slug));
+      }
+      if (_.isPlainObject(this.props.deck)) {
+        if (_.isUndefined(this.props.content)) {
+          backend.relay(fetchContent(this.props.deck.contentId));
+        }
       }
     }
-  }
-  componentDidUpdate() {
-    // console.log('update', this.props.deck, this.props.content);
-    if( this.props.deck === undefined ) {
-      backend.relay(fetchDeck(this.props.slug));
-    }
-    // console.log('update', this.props.deck, this.props.content);
-    if(_.isPlainObject(this.props.deck)) {
-      if( _.isUndefined(this.props.content) ) {
-        backend.relay(fetchContent(this.props.deck.contentId));
+    componentDidUpdate() {
+      // console.log('update', this.props.deck, this.props.content);
+      if (this.props.deck === undefined) {
+        backend.relay(fetchDeck(this.props.slug));
+      }
+      // console.log('update', this.props.deck, this.props.content);
+      if (_.isPlainObject(this.props.deck)) {
+        if (_.isUndefined(this.props.content)) {
+          backend.relay(fetchContent(this.props.deck.contentId));
+        }
       }
     }
+
+    render() {
+      const { content, study } = this.props;
+      const { editing, notes } = this.state;
+      const deck = this.state.deck || this.props.deck;
+      const hasDeck = _.isPlainObject(this.props.deck);
+      const hasContent = _.isPlainObject(content);
+      if (this.props.deck === "failed") return <Redirect to="/" />;
+      if (!hasDeck) return <Loader active />;
+
+      return (
+        <div>
+          <Container style={{ paddingTop: "2em" }}>
+            <Item.Group>
+              <DeckHeader
+                deck={deck}
+                editable={editing === "deck"}
+                onEdit={this.handleEdit}
+                onChange={this.handleChange}
+                notesVisible={notes}
+                mayEdit={!notes && !this.state.study}
+              />
+            </Item.Group>
+
+            {hasContent ? (
+              <DeckBody
+                deckContent={this.props.content}
+                editDeck={editing === "deck"}
+                editNotes={editing === "notes"}
+                showAnnotations={this.state.notes}
+                onEditNotes={this.handleEditNotes}
+                onSave={this.handleSave}
+                onDiscard={this.handleDiscard}
+                ref={this.handleBodyRef}
+              />
+            ) : (
+              <Loader active />
+            )}
+
+            {false && <DeckComments />}
+          </Container>
+          {study && <Study deckId={deck.id} />}
+        </div>
+      );
+    }
   }
-
-  render() {
-    const {content, study} = this.props;
-    const {editing, notes} = this.state;
-    const deck = this.state.deck || this.props.deck;
-    const hasDeck = _.isPlainObject(this.props.deck);
-    const hasContent = _.isPlainObject(content)
-    if( this.props.deck === 'failed')
-      return <Redirect to="/"/>
-    if( !hasDeck )
-      return (<Loader active/>)
-
-    return(
-      <div>
-        <Container style={{paddingTop: "2em"}}>
-          <Item.Group>
-            <DeckHeader
-              deck={deck}
-              editable={editing==='deck'}
-              onEdit={this.handleEdit}
-              onChange={this.handleChange}
-              notesVisible={notes}
-              mayEdit={!notes && !this.state.study}/>
-          </Item.Group>
-
-          { hasContent
-          ? <DeckBody
-            deckContent={this.props.content}
-            editDeck={editing==='deck'}
-            editNotes={editing==='notes'}
-            showAnnotations={this.state.notes}
-            onEditNotes={this.handleEditNotes}
-            onSave={this.handleSave}
-            onDiscard={this.handleDiscard}
-            ref={this.handleBodyRef} />
-          : <Loader active/> }
-
-          { false && <DeckComments/>}
-        </Container>
-        { study && <Study deckId={deck.id}/> }
-      </div>
-    );
-  }
-})
+);
 
 function toNewDeckProps(store) {
   return {
@@ -157,47 +160,62 @@ function toNewDeckProps(store) {
   };
 }
 
-const NewDeck = withRouter(connect(toNewDeckProps)(class NewDeck extends Component {
-  state = {};
-  onSave = () => {
-    const {deck} = this.state;
-    const slug = mkSlug(deck.title);
-    const contentId = uuid();
-    const deckId = uuid();
+const NewDeck = withRouter(
+  connect(toNewDeckProps)(
+    class NewDeck extends Component {
+      state = {};
+      onSave = () => {
+        const { deck } = this.state;
+        const slug = mkSlug(deck.title);
+        const contentId = uuid();
+        const deckId = uuid();
 
-    backend.relay(receiveContent(contentId, this.bodyRef.deckEditorRaw()));
-    backend.relay(receiveDeck({...deck, id: deckId, slugs: [slug], contentId, nLikes: 0, nComments: 0, dirty: true, processing: true}));
+        backend.relay(receiveContent(contentId, this.bodyRef.deckEditorRaw()));
+        backend.relay(
+          receiveDeck({
+            ...deck,
+            id: deckId,
+            slugs: [slug],
+            contentId,
+            nLikes: 0,
+            nComments: 0,
+            dirty: true,
+            processing: true
+          })
+        );
 
-    this.props.history.push('/decks/'+deckId);
-  }
-  handleChange = deck => {
-    this.setState({ deck });
-  }
-  handleBodyRef = (ref) => this.bodyRef = ref;
+        this.props.history.push("/decks/" + deckId);
+      };
+      handleChange = deck => {
+        this.setState({ deck });
+      };
+      handleBodyRef = ref => (this.bodyRef = ref);
 
-  render = () => {
-    const deck = this.state.deck || {title: '', tags: []};
-    return(
-      <div>
-        <Container style={{paddingTop: "2em"}}>
-          <Item.Group>
-            <DeckHeader
-              deck={deck}
-              onChange={this.handleChange}
-              mayEdit={true}
-              editable={true}
+      render = () => {
+        const deck = this.state.deck || { title: "", tags: [] };
+        return (
+          <div>
+            <Container style={{ paddingTop: "2em" }}>
+              <Item.Group>
+                <DeckHeader
+                  deck={deck}
+                  onChange={this.handleChange}
+                  mayEdit={true}
+                  editable={true}
+                />
+              </Item.Group>
+              <DeckBody
+                ref={this.handleBodyRef}
+                editDeck={true}
+                showAnnotations={false}
+                onSave={this.onSave}
               />
-          </Item.Group>
-          <DeckBody
-            ref={this.handleBodyRef}
-            editDeck={true}
-            showAnnotations={false}
-            onSave={this.onSave}
-            />
-        </Container>
-      </div>
-    );
-  }
-}))
+            </Container>
+          </div>
+        );
+      };
+    }
+  )
+);
 
 export { NewDeck };
