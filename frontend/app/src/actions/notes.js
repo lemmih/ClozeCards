608d9dad59398d4e968bce8d5fe9b09d6ac0f652
David Himmelstrup <lemmih@gmail.com>
2017-11-24 16:40:32 +0100

    Upgrade packages, try out flow.

--- frontend/app/src/actions/notes.js
+++ frontend/app/src/actions/notes.js
@@ -1,13 +1,35 @@
+// @flow
 export const FETCH_NOTES = "FETCH_NOTES";
 export const RECEIVE_NOTES = "RECEIVE_NOTES";
 
-export function fetchNotes(userId, deckId) {
+export type FetchNotesAction = {|
+  type: "FETCH_NOTES",
+  payload: {|
+    userId: string,
+    deckId: string
+  |}
+|};
+
+export type ReceiveNotesAction = {|
+  type: "RECEIVE_NOTES",
+  payload: {|
+    userId: string,
+    deckId: string,
+    contentId: string
+  |}
+|};
+
+export function fetchNotes(userId: string, deckId: string): FetchNotesAction {
   return {
     type: FETCH_NOTES,
     payload: { userId, deckId }
   };
 }
-export function receiveNotes(userId, deckId, contentId) {
+export function receiveNotes(
+  userId: string,
+  deckId: string,
+  contentId: string
+): ReceiveNotesAction {
   return {
     type: RECEIVE_NOTES,
     payload: { userId, deckId, contentId }
